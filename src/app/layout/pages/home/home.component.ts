8c0fe8f60bf0fe603a0e558a33e82bf3cdc77ba1
ahmedkhaled124 <www.ahmedmortada441@gmail.com>
2024-09-03 20:53:04 +0300

    session2 week 4

--- src/app/layout/pages/home/home.component.ts
+++ src/app/layout/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  ngOnInit(): void {
+    if( typeof localStorage!= 'undefined')
+   localStorage.setItem('currentpage','/home')
+  }
 
 }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { loginData, RegisterData } from '../../interfaces/data';
 import { Enviroment } from '../../../base/Enviroment';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { jwtDecode } from "jwt-decode";
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Injectable({
@@ -12,7 +13,14 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  constructor(private _HttpClient:HttpClient , private _Router:Router) { }
+  constructor(private _HttpClient:HttpClient , private _Router:Router,@Inject(PLATFORM_ID) id:object) {
+ if(isPlatformBrowser(id)){
+  if(localStorage.getItem('usertoken')){
+    this.deCodeUserData();
+    _Router.navigate([localStorage.getItem('currentpage')])
+  }
+ }
+  }
 
   userData:BehaviorSubject<any> = new BehaviorSubject(null);
 
